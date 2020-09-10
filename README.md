bb8d9f75a7af14d2834e7f6507f6302ce86024ae
billymane <bilalkozak.webdev@gmail.com>
2020-09-10 13:57:19 -0400

    Refactored my code by using querySelectorAll for the numKeys

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # calculator
+
+Calculator project based on The Odin Project.
+
+# how to use
+
+Use your mouse to click on the buttons on the calculator.
+
+# to do list
+
+Keyboard support
--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const output = document.querySelector("output")
-const oneKey = document.querySelector("#one")
-const twoKey = document.querySelector("#two")
-const threeKey = document.querySelector("#three")
-const fourKey = document.querySelector("#four")
-const fiveKey = document.querySelector("#five")
-const sixKey = document.querySelector("#six")
-const sevenKey = document.querySelector("#seven")
-const eightKey = document.querySelector("#eight")
-const nineKey = document.querySelector("#nine")
-const zeroKey = document.querySelector("#zero")
+const numKeys = document.querySelectorAll(".operand")
 const plusKey = document.querySelector("#plus")
 const minusKey = document.querySelector("#minus")
 const multiplyKey = document.querySelector("#multiply")
@@ -50,115 +41,91 @@ let operate = (o, a, b) => {
         } else {
             return divide(a, b);
         }
-    } 
+    }
 }
 
+let display = (num) => {
+    displayValue += num;
+    output.textContent = displayValue;
+}
 
-    let display = (num) => {
-        displayValue += num;
-        output.textContent = displayValue;
+for (const numKey of numKeys) {
+    numKey.addEventListener("click", function() {
+        display(numKey.textContent);
+    })
+}
+
+backspaceKey.addEventListener("click", function () {
+    displayValue = displayValue.slice(0, -1)
+    output.textContent = displayValue;
+})
+
+dotKey.addEventListener("click", function () {
+    if (!displayValue.includes(".")) {
+        display(".");
     }
+});
 
-    oneKey.addEventListener("click", function () {
-        display("1");
-    });
-    twoKey.addEventListener("click", function () {
-        display("2");
-    });
-    threeKey.addEventListener("click", function () {
-        display("3");
-    });
-    fourKey.addEventListener("click", function () {
-        display("4");
-    });
-    fiveKey.addEventListener("click", function () {
-        display("5");
-    });
-    sixKey.addEventListener("click", function () {
-        display("6");
-    });
-    sevenKey.addEventListener("click", function () {
-        display("7");
-    });
-    eightKey.addEventListener("click", function () {
-        display("8");
-    });
-    nineKey.addEventListener("click", function () {
-        display("9");
-    });
-    zeroKey.addEventListener("click", function () {
-        display("0");
-    });
-    backspaceKey.addEventListener("click", function() {
-        displayValue = displayValue.slice(0, 1)
-        output.textContent = displayValue;
-    })
-    dotKey.addEventListener("click", function () {
-        if (!displayValue.includes(".")) {
-            display(".");
-        }
-    });
+let operatorKey = (operateSign) => {
+    firstNum = displayValue;
+    displayValue = "";
+    output.textContent = operateSign;
+    operateValue = operateSign;
+}
 
-    let operatorKey = (operateSign) => {
-        firstNum = displayValue;
-        displayValue = "";
-        output.textContent = operateSign;
-        operateValue = operateSign;
+multiplyKey.addEventListener("click", function () {
+    if (parseInt(firstNum) > 0) {
+        firstNum = operate(operateValue, firstNum, displayValue);
+        displayValue = ""
+        output.textContent = firstNum;
+        operateValue = "*";
+    } else {
+        operatorKey("*");
     }
+});
 
-    multiplyKey.addEventListener("click", function () {
-        if (parseInt(firstNum) > 0) {
-            firstNum = operate(operateValue, firstNum, displayValue);
-            displayValue = ""
-            output.textContent = firstNum;
-            operateValue = "*";
-        } else {
-            operatorKey("*");
-        }
-    });
+plusKey.addEventListener("click", function () {
+    if (parseInt(firstNum) > 0) {
+        firstNum = operate(operateValue, firstNum, displayValue);
+        displayValue = ""
+        output.textContent = firstNum;
+        operateValue = "+";
+    } else {
+        operatorKey("+");
+    }
+});
 
-    plusKey.addEventListener("click", function () {
-        if (parseInt(firstNum) > 0) {
-            firstNum = operate(operateValue, firstNum, displayValue);
-            displayValue = ""
-            output.textContent = firstNum;
-            operateValue = "+";
-        } else {
-            operatorKey("+");
-        }
-    });
+minusKey.addEventListener("click", function () {
+    if (parseInt(firstNum) > 0) {
+        firstNum = operate(operateValue, firstNum, displayValue);
+        displayValue = ""
+        output.textContent = firstNum;
+        operateValue = "-";
+    } else {
+        operatorKey("-");
+    }
+});
 
-    minusKey.addEventListener("click", function () {
-        if (parseInt(firstNum) > 0) {
-            firstNum = operate(operateValue, firstNum, displayValue);
-            displayValue = ""
-            output.textContent = firstNum;
-            operateValue = "-";
-        } else {
-            operatorKey("-");
-        }
-    });
+divideKey.addEventListener("click", function () {
+    if (parseInt(firstNum) > 0) {
+        firstNum = operate(operateValue, firstNum, displayValue);
+        displayValue = ""
+        output.textContent = firstNum;
+        operateValue = "/";
+    } else {
+        operatorKey("/");
+    }
+});
 
-    divideKey.addEventListener("click", function () {
-        if (parseInt(firstNum) > 0) {
-            firstNum = operate(operateValue, firstNum, displayValue);
-            displayValue = ""
-            output.textContent = firstNum;
-            operateValue = "/";
-        } else {
-            operatorKey("/");
-        }
-    });
+equalKey.addEventListener("click", function () {
+    if (isNaN(parseInt(firstNum)) || isNaN(parseInt(displayValue))) {
+    } else {
+        output.textContent = operate(operateValue, firstNum, displayValue);
+    }
+})
 
-    equalKey.addEventListener("click", function () {
-        if (isNaN(parseInt(firstNum)) || isNaN(parseInt(displayValue))) {
-        } else {
-            output.textContent = operate(operateValue, firstNum, displayValue);
-        }
-    })
-
-    clearKey.addEventListener("click", function () {
-        firstNum = "";
-        displayValue = "";
-        output.textContent = "0";
-    })
+clearKey.addEventListener("click", function () {
+    firstNum = "";
+    displayValue = "";
+    output.textContent = "0";
+})
